c9270d04bdbe2694320789ec5218731aed1ee18d
Yahel Tubul <yaeltobol07@gmail.com>
2026-04-29 18:41:47 +0300

    implement the add Edge with using in neighbour list and add documintation

--- Graph.cpp
+++ Graph.cpp
@@ -3,9 +3,15 @@
 // Created by Yahel Tubul on 29/04/2026.
 //
 #include "Graph.h"
-
+// add node in graph
 void Graph::addNode(int id, float x, float y, float z) {
     nodesList[id] = {id, x,y,z}; // allocate values in the node
 }
-
+// add edge from to sides to node in graph
+void Graph::addEdge(int from, int dest, double weight) {
+    //add edge to the first side
+    neighbourList[from].push_back({dest, weight,false});
+    //add edge to the second side
+    neighbourList[dest].push_back({from, weight, false});
+}
 
