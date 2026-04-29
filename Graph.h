3b91b5116e68902e8247d656ca827bdf1bc91155
Yahel Tubul <yaeltobol07@gmail.com>
2026-04-29 15:44:30 +0300

    add sign function for addNode

--- Graph.h
+++ Graph.h
@@ -11,9 +11,11 @@
 
 // This class represent the underground environment computationally
 class Graph {
-    private:
+private:
         std::unordered_map<int, Node> nodesList; // save all nodes in the graph
         std::unordered_map<int, std::vector<Edge>> neighbourList; // save the neighbour of each node
+public:
+        void addNode(int id, int x, int y);
 
 
 
