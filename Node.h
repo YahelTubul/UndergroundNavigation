08a46d5ef2005d887f5399e5b570080be1d8bd10
Yahel Tubul <yaeltobol07@gmail.com>
2026-04-29 15:24:41 +0300

    add struct to present node in the graph

---
+++ Node.h
@@ -0,0 +1,10 @@
+//
+// Created by Yahel Tubul on 29/04/2026.
+//
+
+#ifndef UNDERGROUNDNAVIGATION_NODE_H
+#define UNDERGROUNDNAVIGATION_NODE_H
+struct Node {
+
+};
+#endif //UNDERGROUNDNAVIGATION_NODE_H
