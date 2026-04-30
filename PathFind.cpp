8dfad657444abe80545adc33d99eaf508733bc3a
Yahel Tubul <yaeltobol07@gmail.com>
2026-04-30 11:55:30 +0300

    add  this map to save from which node I came״

--- PathFind.cpp
+++ PathFind.cpp
@@ -14,5 +14,6 @@ std::vector<int> PathFind::findPath(const Graph& graph, int start, int dest) {
         std::greater<>
         > pendingNodes;
     std::unordered_map<int, double> costStart;
+    std::unordered_map<int, int> cameFrom;
 
 }
