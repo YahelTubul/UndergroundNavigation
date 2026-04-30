c67ef8b72dcb05265ab34cdf8581d6471dba6a55
Yahel Tubul <yaeltobol07@gmail.com>
2026-04-30 11:02:05 +0300

    add data structure which contains the nodes to test and from which I will extract the element with the lowest cost

--- PathFind.cpp
+++ PathFind.cpp
@@ -1,3 +1,17 @@
+#include "PathFind.h"
+
+#include <queue>
+#include <vector>
+
+#include "Graph.h"
 //
 // Created by Yahel Tubul on 30/04/2026.
 //
+std::vector<int> PathFind::findPath(const Graph& graph, int start, int dest) {
+    std::priority_queue<
+        std::pair<double,int>,
+        std::vector<std::pair<double,int>>,
+        std::greater<>
+        > pendingNodes;
+
+}
