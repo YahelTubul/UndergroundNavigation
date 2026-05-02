d5a57692373860d79de5f2c805f16663551aed40
Yahel Tubul <yaeltobol07@gmail.com>
2026-05-02 13:59:12 +0300

    add loop to find the path

--- PathFind.cpp
+++ PathFind.cpp
@@ -15,6 +15,10 @@ std::vector<int> PathFind::findPath(const Graph& graph, int start, int dest) {
         > pendingNodes;
     std::unordered_map<int, double> costStart;
     std::unordered_map<int, int> cameFrom;
+    // initialize the start node
     costStart[start] = 0.0;
     pendingNodes.push({0.0,start});
+    while (!pendingNodes.empty()) {
+
+    }
 }
