79ca92ad84f0af64501a8af7220f96376fd2ab1b
Yahel Tubul <yaeltobol07@gmail.com>
2026-05-04 00:12:47 +0300

    add function to set start position from node in the graph

--- LocationEstimator.h
+++ LocationEstimator.h
@@ -4,12 +4,15 @@
 
 #ifndef UNDERGROUNDNAVIGATION_LOCATIONESTIMATOR_H
 #define UNDERGROUNDNAVIGATION_LOCATIONESTIMATOR_H
-
+#include "Graph.h"
 class LocationEstimator {
 private:
     float estimatX = 0, estimatY = 0, estimatZ = 0, Kal_gain = 0;
 public:
+    //constructor that define how much influence has to measure
     LocationEstimator (double gain = 0.6);
+    void initialize(const Graph& graph, int startNode);
+
 
 };
 
