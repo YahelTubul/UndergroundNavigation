377f32d121f274379b481d45707ca24663c37ed6
Yahel Tubul <yaeltobol07@gmail.com>
2026-05-16 14:19:58 +0300

    complete the load graph function

--- GraphLoader.cpp
+++ GraphLoader.cpp
@@ -18,9 +18,8 @@ bool GraphLoader::LoadGraph(const std::string& filePath, Graph& graph) {
     }
     std::string line;
     // read the file
-    while (std::getline(file, line))
-    {
-       //conver the line into stream for parsing
+    while (std::getline(file, line)) {
+        //conver the line into stream for parsing
         std::stringstream ss(line);
         std::string type;
         // read the first word in the line
@@ -37,4 +36,24 @@ bool GraphLoader::LoadGraph(const std::string& filePath, Graph& graph) {
             // add the node into the graph
             graph.addNode(id, name, x, y, z);
         }
+        else if (type == "EDGE")
+        {
+            int from,to;
+            double weight;
+            // example line: EDGE 1 2 5
+            ss >> from >> to >> weight;
+            // add weight connection between 2 nodes
+            graph.addEdge(from, to, weight);
+        }
+        else if (type == "BLOCK")
+        {
+            int from,to;
+            // example line: BLOCK 3 8
+            ss >> from >> to;
+            // mark the edge as block
+            graph.block(from, to);
+        }
+    }
+    file.close();
+    return true;
 }
