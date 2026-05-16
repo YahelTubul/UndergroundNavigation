50bf6f1308a543977a7e5938141bb5f6ce2bbb46
Yahel Tubul <yaeltobol07@gmail.com>
2026-05-16 14:31:30 +0300

    change the main the dont includes function only objects and exiting files

--- main.cpp
+++ main.cpp
@@ -2,50 +2,24 @@
 #include "Graph.h"
 #include "ConfigManager.h"
 #include "Controller.h"
+#include <GraphLoader.h>
 
 
 int main() {
     Graph graph;
-
-    // Nodes: id, x, y, z
-    graph.addNode(1, 0, 0, 0);
-    graph.addNode(2, 1, 0, 0);
-    graph.addNode(3, 2, 0, 0);
-    graph.addNode(4, 0, 1, 0);
-    graph.addNode(5, 1, 1, 0);
-    graph.addNode(6, 2, 1, 0);
-    graph.addNode(7, 0, 2, 0);
-    graph.addNode(8, 1, 2, 0);
-    graph.addNode(9, 2, 2, 0);
-
-    // Edges: from, to, weight
-    graph.addEdge(1, 2, 2);
-    graph.addEdge(2, 3, 2);
-
-    graph.addEdge(1, 4, 4);
-    graph.addEdge(2, 5, 3);
-    graph.addEdge(3, 6, 2);
-
-    graph.addEdge(4, 5, 3);
-    graph.addEdge(5, 6, 2);
-
-    graph.addEdge(4, 7, 3);
-    graph.addEdge(5, 8, 2);
-    graph.addEdge(6, 9, 3);
-
-    graph.addEdge(7, 8, 1);
-    graph.addEdge(8, 9, 2);
-    // blocks
-
-    graph.block(2, 3);
-    graph.block(5, 6);
+    //load the graph from exiting file
+    bool graphLoaded =GraphLoader::LoadGraph("../data/graph.txt",graph);
+    //check if loading file is failed
+    if (!graphLoaded)
+    {
+        std::cout << "Graph loading failed!" << std::endl;
+        return 1;
+    }
+    //load the kalman gain from exiting file
     double gain = ConfigManager::LoadGain("../config/settings.txt");
-
+    std::cout << "loaded gain: "<< gain << std::endl;
     Controller controller(graph, gain);
-    int startNode = 1;
-    int destinationNode = 9;
 
-    controller.navigation(startNode, destinationNode);
 
     return 0;
 }
