bf423558c9d0f4f0bb46a251fdaa7043ed67e14e
Yahel Tubul <yaeltobol07@gmail.com>
2026-05-16 14:00:36 +0300

    create class to load the graph from exiting file

---
+++ GraphLoader.h
@@ -0,0 +1,11 @@
+//
+// Created by Yahel Tubul on 16/05/2026.
+//
+
+#ifndef UNDERGROUNDNAVIGATION_GRAPHLOADER_H
+#define UNDERGROUNDNAVIGATION_GRAPHLOADER_H
+class GraphLoader {
+public:
+    static bool LoadGraph(const std::string& filePath, Graph& graph);
+};
+#endif //UNDERGROUNDNAVIGATION_GRAPHLOADER_H
